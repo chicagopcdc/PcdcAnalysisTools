8659f2dfe70c80c04d39b08b21e6f05278c24ee7
grugna <graglia01@gmail.com>
2021-04-05 09:26:54 -0500

    add GDC connection

---
+++ PcdcAnalysisTools/blueprint/routes/views/external/__init__.py
@@ -0,0 +1,57 @@
+import json
+import flask
+import urllib.parse
+from flask import current_app as capp
+
+
+from PcdcAnalysisTools import utils
+from PcdcAnalysisTools import auth
+
+
+@auth.authorize_for_analysis("access")
+def get_gdc():
+    print("IN GDC - LUCA")
+
+    args = utils.parse.parse_request_json()
+    data = (
+        fetch_data(args)
+        if flask.current_app.config.get("IS_SURVIVAL_USING_GUPPY", True)
+        else fetch_fake_data(args)
+    )
+
+    query = '{"op":"and","content":[{"op":"in","content":{"field":"cases.case_id","value":'
+    query += json.dumps(data)
+    query += '}}]}'
+
+    encoded = urllib.parse.quote(query)
+    link = 'https://portal.gdc.cancer.gov/exploration?filters=' + encoded
+
+    return flask.jsonify({"link": link})
+
+
+
+def fetch_data(args):
+    # TODO add json payload control
+    # TODO add check on payload nulls and stuff
+    # TODO add path in the config file or ENV variable
+    _filter = args.get("filter")
+    filters = json.loads(json.dumps(_filter))
+
+    fields = ["external_subject_id"]
+
+    guppy_data = utils.guppy.downloadDataFromGuppy(
+        path="http://guppy-service/download",
+        type="subject",
+        totalCount=100000,
+        fields=fields,
+        filters=filters,
+        sort=[],
+        accessibility="accessible",
+        config=capp.config
+    )
+
+    # TODO I can count how many are without that information and communicate that to the frontend (guppy data return empty objects when data is missing)
+    ids = [item["external_subject_id"] for item in guppy_data if "external_subject_id" in item]
+
+    return ids
+
