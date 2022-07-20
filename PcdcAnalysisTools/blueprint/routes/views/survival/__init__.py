7443c483e5bc4e9cefde0104f7521897c6ab7751
Bobae Kang <bkang.dev@gmail.com>
2022-07-20 16:47:41 -0500

    Add missing parenthesis

--- PcdcAnalysisTools/blueprint/routes/views/survival/__init__.py
+++ PcdcAnalysisTools/blueprint/routes/views/survival/__init__.py
@@ -122,7 +122,7 @@ def fetch_data(filters, efs_flag):
         pd.DataFrame.from_records(guppy_data)
         .assign(
             omitted=lambda x:
-                ((x[status_var].isna()) | x[status_var] == 'Unknown' |
+                ((x[status_var].isna()) | (x[status_var] == 'Unknown') |
                  (x[time_var].isna()) | (x[time_var] < 0)),
             status=lambda x:
                 np.where(x["omitted"], None, x[status_var] == status_str),
