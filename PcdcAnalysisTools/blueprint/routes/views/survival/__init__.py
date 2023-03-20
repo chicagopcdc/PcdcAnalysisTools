80a2ebf46a6e230ff6f5dd75bc1b1a9ec16e1766
Luca Graglia <graglia01@gmail.com>
2023-03-20 13:32:29 -0500

    update comment

--- PcdcAnalysisTools/blueprint/routes/views/survival/__init__.py
+++ PcdcAnalysisTools/blueprint/routes/views/survival/__init__.py
@@ -273,7 +273,7 @@ def check_allowed_filter(config, filter_set):
         if len(path) == 1:
             value = path[0]
         elif len(path) > 1:
-            # This may be a problem if a variable in another path has the same name as the one in this path
+            # TODO This may be a problem if a variable in another path has the same name as the one in this path. No variables used as of now have this issue, so this is a reminder for later.
             value = path[-1]
         else:
             raise UnsupportedError("Something in wrong in the survival exluded variable configuration, please notify the admins at pcdc_help@lists.uchicago.edu")
