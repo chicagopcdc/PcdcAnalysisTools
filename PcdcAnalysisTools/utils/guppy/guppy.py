2b2f9dbaf2451ddf213020bd8a51987b459dc18b
pkellyc <pkellyc@PED-ML-31.local>
2025-04-23 21:37:51 -0500

    DownloadDataFromGuppy for standard signing

--- PcdcAnalysisTools/utils/guppy/guppy.py
+++ PcdcAnalysisTools/utils/guppy/guppy.py
@@ -5,6 +5,8 @@ from PcdcAnalysisTools.auth import get_jwt_from_header
 from pcdcutils.signature import SignatureManager
 from pcdcutils.errors import NoKeyError
 from pcdcutils.helpers import encode_str
+from pcdcutils.gen3 import Gen3RequestManager
+from types import SimpleNamespace
 
 
 def downloadDataFromGuppy(path, type, totalCount, fields, filters, sort, accessibility, config):
@@ -22,22 +24,36 @@ def downloadDataFromGuppy(path, type, totalCount, fields, filters, sort, accessi
             queryBody["accessibility"] = 'accessible' # accessibility
 
         try:
-            # TODO in the future use the makesignature in the gen3Manager in pcdcutils to make this
-            url = path #'http://guppy-service/download'
+            url = path  # path should be assigned properly
             headers = {'Content-Type': 'application/json'}
             body = json.dumps(queryBody, separators=(',', ':'))
-            # Use this only for the signature because json.dumps will transform special char in \u encoding which will make a signature javascript won't be able to verify it.
-            # For instance `β` is translated to `\u03b2`
-            body_signature = json.dumps(queryBody, separators=(',', ':'), ensure_ascii=False)
             jwt = get_jwt_from_header()
-            headers['Authorization'] = 'bearer ' + jwt
-            sm = SignatureManager(key=config["RSA_PRIVATE_KEY"])
-            headers['Signature'] = 'signature ' + sm.sign(body_signature)
-            headers['Gen3-Service'] = encode_str(config.get('SERVICE_NAME'))
 
-            r = requests.post(
-                url, data=body, headers=headers # , proxies=flask.current_app.config.get("EXTERNAL_PROXIES")
+            # Instance with the req Gen3 headers
+            mgr = Gen3RequestManager({
+                'Signature': None,
+                'Gen3-Service': config['SERVICE_NAME']
+            })
+
+            # Using pcdcutils gen3 make_gen3_signature
+            signature = mgr.make_gen3_signature(
+                payload=SimpleNamespace(
+                    method='POST',  # or any method you're using
+                    path=url,
+                    get_data=lambda as_text=True: body  # Pass body as text
+                ),
+                config=config
             )
+
+            # headers
+            headers['Signature'] = 'signature ' + signature.decode()  # Ensure it's decoded if needed
+            headers['Gen3-Service'] = encode_str(config['SERVICE_NAME'])
+
+            if jwt:
+                headers['Authorization'] = 'bearer ' + jwt
+
+            r = requests.post(url, data=body, headers=headers)
+                
         except NoKeyError as e:
             print(e.message)
             return []
